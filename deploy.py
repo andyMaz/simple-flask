231d9a8dbec18d113ba2e3bb82c55e874fa1e1d2
Andrei Rika <andrei.rika@gmail.com>
2018-08-05 22:47:22 +0100

    initial commit

---
+++ deploy.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "<h1>Deploying simple Flask to Heroku</h1>"
+
+if __name__ == "__main__":
+  app.run(debug=True)
